e7a1bf7823848989bf4736c0c9d6ff56625e8706
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-10-22 13:34:08 +0800

    Program finished. Nickname was fully added to the code.

--- nicknameasterisk.py
+++ nicknameasterisk.py
@@ -3,19 +3,14 @@
 # Your program should be uploaded to github before 1:30 pm
 
 for row in range(7):
-    for col in range(30):
-        if(row!=0 and row!= 6 and (col==0 )) or ((row==0 or row==6) and (col > 0 and col< 5)) or \
-          ((col==6 or col == 10) and row!=0) or ((row==0 or row ==3) and (col > 6 and col < 10)) or \
+    for col in range(29):
+        if(row!=0 and row!= 6 and (col==0 )) or ((row==0 or row == 6) and (col > 0 and col< 5)) or \
+          ((col==6 or col == 10) and row!=0) or ((row==0 or row == 3) and (col > 6 and col < 10)) or \
           (col== 12 or (row == 6 and col > 12 )) and (col < 17) or \
-          ((row == 0 or row == 6) and (col > 18 and col < 22 )) or ((col== 18 or col== 22) and (row > 0 and row < 6)):
-          
+          ((row == 0 or row == 6) and (col > 18 and col < 22 )) or ((col== 18 or col == 22) and (row > 0 and row < 6)) or \
+          (col == 26 and row > 1) or (row==0 and col == 28) or (row == 1 and (col == 25 or col == 27)) or (row== 0 and (col==24 or col == 28)):
             
             print("*",end=" ")
         else:
             print(end="  ")
     print( )
-
- #   
-
-
-
